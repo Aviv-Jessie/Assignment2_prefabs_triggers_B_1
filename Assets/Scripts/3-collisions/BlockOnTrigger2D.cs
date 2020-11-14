60576bbcccce35e13c90d1855254efb4fd75a62e
aviv1620 <44925669+aviv1620@users.noreply.github.com>
2020-11-14 23:55:42 +0200

    first commit

---
+++ Assets/Scripts/3-collisions/BlockOnTrigger2D.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockOnTrigger2D : MonoBehaviour
+{
+    enum Limit {Left,Right,Top,Buttom}
+
+    [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
+    [SerializeField] string triggeringTag = "Player";
+    [SerializeField] float defiance = 0.5f;
+    [SerializeField] Limit limit;
+    private void OnTriggerEnter2D(Collider2D other) {       
+        if (other.tag == triggeringTag && enabled) {
+            switch (limit)
+            {
+                case Limit.Top:
+                    other.gameObject.transform.position += Vector3.down * defiance;
+                    break;
+                case Limit.Buttom:
+                    other.gameObject.transform.position += Vector3.up * defiance;
+                    break;
+                case Limit.Left:
+                    other.gameObject.transform.position += Vector3.right * defiance;
+                    break;
+                case Limit.Right:
+                    other.gameObject.transform.position += Vector3.left * defiance;
+                    break;                
+            }
+           
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# TODO list
+Assets\Scripts\3-collisions\BlockOnTrigger2D.cs add explanation and Tooltip
+Assets\Scripts\3-collisions\DestroyOnTrigger2D.cs add explanation and Tooltip
+
+#explanation what we do
+Assets\Scenes\4-level-1 add 
+
+##explanation Credits
+
+Programming:
+* Maoz Grossman
+* Erel Segal-Halevi
+
+Online courses:
+* [The Ultimate Guide to Game Development with Unity 2019](https://www.udemy.com/the-ultimate-guide-to-game-development-with-unity/), by Jonathan Weinberger
+
+Graphics:
+* [Matt Whitehead](https://ccsearch.creativecommons.org/photos/7fd4a37b-8d1a-4d4c-80a2-4ca4a3839941)
+* [Kenney's space kit](https://kenney.nl/assets/space-kit)
+* [Ductman's 2D Animated Spacehips](https://assetstore.unity.com/packages/2d/characters/2d-animated-spaceships-96852)
+* [Franc from the Noun Project](https://commons.wikimedia.org/w/index.php?curid=64661575)
+* [Greek-arrow-animated.gif by Andrikkos is licensed under CC BY-SA 3.0](https://search.creativecommons.org/photos/2db102af-80d0-4ec8-9171-1ac77d2565ce)
